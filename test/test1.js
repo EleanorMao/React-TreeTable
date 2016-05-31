740e0590e85b7dd2539e43b57c34238a699e1766
Elly <danningmao@outlook.com>
2016-06-01 01:53:57 +0800

    add util test

---
+++ test/test1.js
@@ -0,0 +1,13 @@
+/**
+ * Created by elly on 16/6/1.
+ */
+import React from 'react';
+import { expect } from 'chai';
+import { shallow, mount, render } from 'enzyme';
+import TreeTable from '../src/Index';
+
+describe("Shallow TreeTable", function() {
+    it("work well when data is empty", function() {
+        expect(shallow(<TreeTable data={[]}/>).find('.table-row').text()).to.equal('暂无数据');
+    });
+});
